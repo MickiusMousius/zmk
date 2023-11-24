11ee8e6b69522cf7b376f51e2e5c36afb8085b53
MickiusMousius <MickiusMousius@icloud.com>
2023-11-24 18:31:28 +1030

    Fix battery percentage alignment

--- app/boards/shields/nice_view/widgets/util.c
+++ app/boards/shields/nice_view/widgets/util.c
@@ -26,7 +26,7 @@ void rotate_canvas(lv_obj_t *canvas, lv_color_t cbuf[]) {
 
 void draw_battery(lv_obj_t *canvas, const struct status_state *state) {
     lv_draw_label_dsc_t label_dsc;
-    init_label_dsc(&label_dsc, LVGL_FOREGROUND, &lv_font_montserrat_16, LV_TEXT_ALIGN_RIGHT);
+    init_label_dsc(&label_dsc, LVGL_FOREGROUND, &lv_font_montserrat_16, LV_TEXT_ALIGN_LEFT);
 
     lv_draw_rect_dsc_t rect_black_dsc;
     init_rect_dsc(&rect_black_dsc, LVGL_BACKGROUND);
@@ -47,7 +47,7 @@ void draw_battery(lv_obj_t *canvas, const struct status_state *state) {
 
     char charge_text[6] = {};
     snprintf(charge_text, sizeof(charge_text), "%d%%", state->battery);
-    lv_canvas_draw_text(canvas, 20, 0, 60, &label_dsc, charge_text);
+    lv_canvas_draw_text(canvas, 37, 0, 60, &label_dsc, charge_text);
 }
 
 void init_label_dsc(lv_draw_label_dsc_t *label_dsc, lv_color_t color, const lv_font_t *font,
